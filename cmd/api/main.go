0f5080f752c8c58a124e79376b32d53672762913
André devkcud Albanese <patommmmm@proton.me>
2023-10-09 20:27:16 -0300

    feat(connection): Establish connection and insert a document

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const mongoURL string = "mongodb://localhost:27017"
 
 func main() {
-    fmt.Println("Hello, world!")
+	// Establish a connection to MongoDB
+	clientOptions := options.Client().ApplyURI(mongoURL)
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connected to MongoDB!")
+
+	// Create a collection
+	collection := client.Database("test").Collection("users")
+
+	// Insert a document
+	result, err := collection.InsertOne(context.TODO(), bson.D{
+		{"name", "Pato"},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Inserted a single document: ", result.InsertedID)
+
+	// Disconnect from MongoDB
+	defer client.Disconnect(context.TODO())
+	log.Println("Disconnected from MongoDB!")
 }
